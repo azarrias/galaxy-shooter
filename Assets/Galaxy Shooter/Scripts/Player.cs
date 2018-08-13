bdd2d0bf92ab676d1012b7954cbd63470bcfb213
azarrias <adolfo.zarrias@gmail.com>
2018-08-13 17:30:25 +0200

    Add player ship bounds

--- Assets/Galaxy Shooter/Scripts/Player.cs
+++ Assets/Galaxy Shooter/Scripts/Player.cs
@@ -4,17 +4,43 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-    public float speed = 5.0f;
+    [SerializeField]
+    private float speed = 5.0f;
 
 	void Start () {
         transform.position = new Vector3(0, 0, 0);
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
+
         transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
         transform.Translate(Vector3.up * speed * verticalInput * Time.deltaTime);
+
+        if (transform.position.x > 8.15f)
+        {
+            transform.position = new Vector3(8.15f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -8.15f)
+        {
+            transform.position = new Vector3(-8.15f, transform.position.y, 0);
+        }
+
+        if (transform.position.y > 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y < -4.2f)
+        {
+            transform.position = new Vector3(transform.position.x, -4.2f, 0);
+        }
     }
 }
