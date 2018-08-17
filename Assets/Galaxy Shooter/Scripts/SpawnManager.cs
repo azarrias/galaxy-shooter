3d663e8284594f88764decfdcf45b000ff724b22
azarrias <adolfo.zarrias@gmail.com>
2018-08-17 21:49:31 +0200

    Spawn Manager handles creation of enemies and powerups

---
+++ Assets/Galaxy Shooter/Scripts/SpawnManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject enemyShipPrefab;
+    [SerializeField]
+    private GameObject[] powerups;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(SpawnEnemy());
+        StartCoroutine(SpawnPowerup());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator SpawnEnemy()
+    {
+        while (true)
+        {
+            Instantiate(enemyShipPrefab, new Vector3(Random.Range(-7f, 7f), 7, 0), Quaternion.identity);
+            yield return new WaitForSeconds(5.0f);
+        }
+    }
+
+    IEnumerator SpawnPowerup()
+    {
+        while (true)
+        {
+            int randomPowerup = Random.Range(0, 3);
+            Instantiate(powerups[randomPowerup], new Vector3(Random.Range(-7f, 7f), 7, 0), Quaternion.identity);
+            yield return new WaitForSeconds(5.0f);
+        }
+    }
+}
