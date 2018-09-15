95ebbe08dd90fb3beb208939d73b7e7f0f54210e
azarrias <adolfo.zarrias@gmail.com>
2018-09-15 19:35:15 +0200

    Add player animations

---
+++ Assets/Galaxy Shooter/Scripts/PlayerAnimation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour {
+
+    private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            animator.SetBool("TurnRight", false);
+            animator.SetBool("TurnLeft", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            animator.SetBool("TurnRight", false);
+            animator.SetBool("TurnLeft", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            animator.SetBool("TurnLeft", false);
+            animator.SetBool("TurnRight", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            animator.SetBool("TurnLeft", false);
+            animator.SetBool("TurnRight", false);
+        }
+
+    }
+}
